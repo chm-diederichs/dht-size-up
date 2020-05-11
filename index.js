26739ebc904a915860d35f726f4e96941ae37e28
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-05-11 21:34:28 +0200

    tidy

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const crypto = require('crypto')
 const dht = require('dht-rpc')
-// const distance = require('xor-distance')
 
 var key = shasum(Date.now().toString(16))
 
-var test = Buffer.from('d25df0e0420aefd3b7142d892d0bf38f09a7575504aee700fac313a873dc23f8', 'hex')
-var target = Buffer.from('cca010e60d853f6d8be93c1752de18daea2112caae81e3c6ca98dde139fb998f', 'hex')
-
-var ref = shasum('reference')
-
 var bootstrap = dht({ ephemeral: true })
 bootstrap.listen(10001)
-
-var tests = []
-for (let i = 0; i < 10000; i++) {
-  tests.push(key)
-  key = shasum(key)
-}
 
 var pops = []
 
@@ -24,11 +12,19 @@ coeffs = new Array(256)
 coeffs[0] = 0
 for (let i = 1; i < 256; i++) coeffs[i] = coeffs[i - 1] + coeff(256, i, 0.5)
 
-for (let i = 0; i < 1000; i++) {
-  pops.push(findVolume(tests.map(a => xorDistance(a, test)).sort((a, b) => a - b).slice(0, 20)))
-  test = shasum(test)
-}
-console.log(pops.reduce((acc, a) => acc + a) / pops.length)
+// TEST WITH HASHES
+
+// var tests = []
+// 
+// for (let i = 0; i < 10000; i++) {
+//   tests.push(key)
+//   key = shasum(key)
+// }
+// for (let i = 0; i < 1000; i++) {
+//   pops.push(findVolume(tests.map(a => xorDistance(a, test)).sort((a, b) => a - b).slice(0, 20)))
+//   test = shasum(test)
+// }
+// console.log(pops.reduce((acc, a) => acc + a) / pops.length)
 
 createNodes(1000)
 
@@ -86,11 +82,6 @@ function estimatePopulation (arr) {
   return arr.length / coeffs[furthest]
 }
 
-function bufferNotPresentIn (buf, arr) {
-  if (!arr.length) return true
-  return arr.findIndex(b => Buffer.compare(buf, b) === 0) < 0
-}
-
 function shasum (data) {
   return crypto.createHash('sha256').update(data).digest()
 }
@@ -109,10 +100,6 @@ function bufferBitCount (buf) {
   }
 
   return count
-}
-
-function sortXorDistance (ref) {
-  return (a, b) => xorDistance(a, ref) - xorDistance(b, ref)
 }
 
 function xorDistance (a, b) {
@@ -134,21 +121,4 @@ function bufferXor (a, b) {
   }
 
   return short
-}
-
-function closest (nodes, key) {
-  var min = Number.MAX_SAFE_INTEGER
-  ret = null
-
-  var tmp
-  for (let i = 0; i < nodes.length; i++) {
-    tmp = xorDistance(nodes[i], key)
-    if (tmp < min) {
-      min = tmp
-      ret = nodes[i]
-    }
-  }
-
-
-  return ret
 }
