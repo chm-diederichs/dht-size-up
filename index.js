7db6e80b58fa17f9c54f71aa5a5ee95b9d88650d
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-05-12 10:20:37 +0200

    change xor distance metric

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ bootstrap.listen(10001)
 var pops = []
 
 coeffs = new Array(256)
-coeffs[0] = 0
-for (let i = 1; i < 256; i++) coeffs[i] = coeffs[i - 1] + coeff(256, i, 0.5)
+var test = Buffer.alloc(32)
+
+// for (let i = 1; i < 256; i++) coeffs[i] = coeffs[i - 1] + 0.5 ** (i + 1)
+console.log(coeffs)
 
 // TEST WITH HASHES
 
 // var tests = []
-// 
-// for (let i = 0; i < 10000; i++) {
+
+// for (let i = 0; i < 100; i++) {
 //   tests.push(key)
 //   key = shasum(key)
 // }
+
 // for (let i = 0; i < 1000; i++) {
-//   pops.push(findVolume(tests.map(a => xorDistance(a, test)).sort((a, b) => a - b).slice(0, 20)))
+//   pops.push(estimatePopulation(tests.map(a => xorDistance(a, test)).sort(Buffer.compare).slice(0, 20)))
 //   test = shasum(test)
 // }
-// console.log(pops.reduce((acc, a) => acc + a) / pops.length)
+// console.log(Number(pops.reduce((acc, a) => acc + a)) / pops.length)
 
 createNodes(1000)
 
@@ -34,7 +37,7 @@ function createNodes (n) {
 
   node.on('ready', function () {
     nodes.push(node)
-    if (n === 0) return query(key, 999)
+    if (n === 0) return query(key, 1000)
     return createNodes(--n)
   })
 }
@@ -43,9 +46,13 @@ function query (find, n) {
   console.log(n)
   var distances = []
 
-  if (n === 0) return console.log(pops.reduce((acc, a) => acc + a) / pops.length)
+  if (n === 0) {
+    console.log(pops)
+    console.log(pops.reduce((acc, a) => acc + a) / BigInt(pops.length))
+    return
+  }
 
-  nodes[n].query('_find_node', find)
+  bootstrap.query('_find_node', find)
     .on('data', function (data) {
       distances.push(xorDistance(data.node.id, find))
     })
@@ -78,8 +85,13 @@ function coeff (n, k, p) {
 }
 
 function estimatePopulation (arr) {
-  const furthest = Math.max(...arr)
-  return arr.length / coeffs[furthest]
+  var sorted = arr.sort(Buffer.compare)
+  const furthest = BigInt('0x' + sorted.pop().toString('hex'))
+
+  const probability = Number(2n ** 256n / furthest)
+  var est1 =  BigInt(arr.length * probability)
+
+  return est1
 }
 
 function shasum (data) {
@@ -102,8 +114,12 @@ function bufferBitCount (buf) {
   return count
 }
 
+function bigIntSub (a, b) {
+  return BigInt(a) - BigInt(b)
+}
+
 function xorDistance (a, b) {
-  return bufferBitCount(bufferXor(a, b))
+  return bufferXor(a, b)
 }
 
 function bufferXor (a, b) {
